61dcb18d51bfa26b157466773ec7883fa292bc9e
matutosi <matutosi@gmail.com>
2022-11-21 16:52:58 +0900

    update

--- docs/biodiv/js2/ul_module.js
+++ docs/biodiv/js2/ul_module.js
@@ -86,8 +86,8 @@ function createSelectLayer(id){
   var span = crEl({ el:'span', ih: 'Layer:', ats:{id:id} });
   var tables = document.querySelectorAll("table[id^='input_occ']");
   var ly = 'Layer';
-  var layer_list = uniq(getMultiTableInputs(tables, [ly])[ly]);
-  var layer_select = createSelectOpt(layer_list, 0);
+  var layer_list = uniq(getMultiTableOptions(tables, [ly])[ly]);
+  var layer_select = createSelectOpt(layer_list, layer_list.length - 1);
   span.appendChild(layer_select)
   return span;
 }
@@ -163,86 +163,3 @@ function getChildrenValues(element){
   return values;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// plot select
-function createPlotSelect(tables, pl = 'PLOT'){
-  var plot_list = uniq(getMultiTableInputs(tables, [pl])[pl]);
-  var plot_select = createSelectOpt(plot_list, 0);
-  plot_select.id = 'plot_select';
-  return plot_select;
-}
-// layer select
-function createLayerSelect(tables, ly = 'Layer'){
-  var layer_list = uniq(getMultiTableOptions(tables, [ly])[ly]);
-  var layer_select = createSelectOpt(layer_list, layer_list.length - 1);
-  layer_select.id = 'layer_select';
-  return layer_select;
-}
-
-
-
-
-
-// Helper functions for generateSpeciesTable()
-// species list
-function createSpeciesListTable(species, table_name, n=15){
-  var table = crEl({ el: 'table', ats: {id: table_name} });
-  var ncol_sp = Math.ceil(species.length / n);
-  var c_names = [];
-  for(i = 0; i < ncol_sp; i++){ c_names.unshift('Species'); }
-  addThTr(table, c_names);
-  var tr = crEl({ el: 'tr' });
-  var td = crEl({ el: 'td'} );
-  for(let j = 0; j < species.length; j++){
-    td.appendChild( createSpeciesButton({ sp: sp_list[j], to_stage: true }) );
-    td.appendChild( crEl({ el: 'br' }) );
-    if( ((j+1) % n) === 0){
-      tr.appendChild(td);
-      if( species.length !== j+1 ){ var td = crEl({ el: 'td'} ); }
-    }
-  }
-  if(n * ncol_sp !== species.length){ tr.appendChild(td); }
-  table.appendChild(tr);
-  return table;
-}
-
-
-function generateSpeciesTable(species){
-  var main = crEl({ el: 'span', ats: {id: 'species_list'} });
-  main.appendChild( crEl({ el:'input', ats:{type:'button', id:'update_input_species_list', value: 'Update input species list', onclick: 'updateSpeciesList()'} }) );
-
-  // species list
-  main.appendChild(createSpeciesListTable(species, 'sp_list', 15));
-
-  // species staged
-  var sp_staged = crEl({ el: 'span', ats:{ id: 'sp_staged'} })
-  main.appendChild(sp_staged);
-  main.appendChild( crEl({ el: 'br' }) );
-  var placeholder = "Input species (separate with ',')";
-  main.appendChild( crEl({ el:'input', ats:{type: 'text', id: 'sp_input', placeholder: placeholder, size:'100'} }) );
-  main.appendChild( crEl({ el: 'br' }) );
-
-  // plot, layer, add
-  var tables = document.querySelectorAll("table[id^='input_occ']");
-  main.appendChild( crEl({ el:'span', ih: 'PLOT:' }) );
-  main.appendChild( createPlotSelect(tables) );
-  main.appendChild( crEl({ el:'span', ih: ', ' }) );
-  main.appendChild( crEl({ el:'span', ih: 'Layer:' }) );
-  main.appendChild( createLayerSelect(tables) );
-  main.appendChild( crEl({ el:'input', ats:{type: 'button', value: 'Add species', onclick: 'addSpecies(this)' } }) );
-
-  main.appendChild( crEl({ el: 'hr' }) );
-  return main;
-}
